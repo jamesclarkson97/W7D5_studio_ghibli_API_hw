6fb7a5928cc0f406c807af55e452cef232804af2
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-08-02 10:45:04 +0100

    Initial Commit. Got data onto the page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <film-select :films="films"/>
+    <p>HEllo WOEOLS</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FilmSelect from './components/FilmSelect.vue';
+import FilmDetail from './components/FilmDetail.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      films: []
+    }
+  },
+  mounted() {
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response => response.json())
+    .then(data => this.films = data)
+    console.log(this.films)
+  },
   components: {
-    HelloWorld
+    "film-select": FilmSelect,
+    "film-detail": FilmDetail,
   }
 }
 </script>
---
+++ src/components/FilmSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <select @change="handleChange" v-model="selectedFilm">
+    <option v-for="(film, index) in films" :film="film" :key="index" v-bind:value="film">{{film.title}}</option>
+  </select>
+</div>
+</template>
+
+<script>
+import {eventBus} from '@/main.js';
+
+export default {
+  name: 'film-select',
+  data() {
+    return {
+      selectedfilm: null
+    };
+  },
+  props: ['films'],
+  methods: {
+    handleChange() {
+      eventBus.$emit('film-select', this.selectedFilm)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
